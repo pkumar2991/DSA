71b90a432732409cd888ab3316e5b961ba55bbfb
prabhakar <techykumar29@gmail.com>
2024-10-13 14:29:40 +0530

    Fixed the incorrect implementation of solution

--- zcodes/DSA_Execercise/src/com/dsa/slidingwindow/MaxofAllSubArray.java
+++ zcodes/DSA_Execercise/src/com/dsa/slidingwindow/MaxofAllSubArray.java
@@ -1,27 +1,39 @@
 package com.dsa.slidingwindow;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class MaxofAllSubArray {
 
 	// Find largest number from each sub-array.
 	public static void main(String[] args) {
-		int[] arr = {1,3,-2,5,9,1};
+		int[] arr = {1,3,-2,5,9,4};
 		int k = 3;
 		int size = arr.length;
 		int i = 0;
 		int j = 0;
-		int max = Integer.MIN_VALUE;
-		
-		while(j < size) {
-			max = Math.max(max, arr[j]);
-			if(j - i + 1 == k) {
-				System.out.println("Maximum of subarray: " + max);
-				if(arr[i] == max) {
-					max = Integer.MIN_VALUE;
+
+		Queue<Integer> queue = new ArrayDeque<>();
+
+		while (j < size){
+			if(queue.isEmpty()){
+				queue.offer(arr[j]);
+			}else if(queue.peek() < arr[j]){
+				queue.clear();
+				queue.offer(arr[j]);
+			}else{
+				queue.offer(arr[j]);
+			}
+			if(j-i+1 == k){
+				System.out.println( queue.peek());
+				if(queue.peek() == arr[i]){
+					queue.poll();
 				}
 				i++;
 			}
 			j++;
 		}
+
 				
 	}
 }
